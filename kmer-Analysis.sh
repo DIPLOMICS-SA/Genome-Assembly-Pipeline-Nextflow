5f8ea427184a3b9a2836e21780f9999d4f8aaf7d
tinameiring <christinameiring@gmail.com>
2025-03-12 15:21:10 +0200

    Moved module loading to the top of the script and added module load KMC

--- kmer-Analysis.sh
+++ kmer-Analysis.sh
@@ -10,6 +10,10 @@
 
 set -e
 
+# Add R module (includes appropriate openMPI and gcc modules)
+module load R
+module load KMC
+
 file=Spekboom_unfragmented.fastq
 kmer=21
 
@@ -17,9 +21,6 @@ echo "${file}" > total_number_bases.txt
 cat "${file}" | awk 'NR%4==2 {sum+=length($0)} END {print sum}' >> total_number_bases.txt
 kmc -k${kmer} -fq -t21 ${file} ${kmer}mers .
 kmc_tools transform ${kmer}mers histogram kmer${kmer}_histo.txt
-
-# Add R module (includes appropriate openMPI and gcc modules)
-module load R/4.2.0
 
 # Run R script
 Rscript kmerPlot.R
